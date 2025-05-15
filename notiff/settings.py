b780b27c693873c42764d350a78499708c880e8b
nico ´ㅅ` <arruenicole@gmail.com>
2025-05-15 10:53:46 -0400

    Update settings.py

--- notiff/settings.py
+++ notiff/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-_(lg@t-&4o3_i-91-hl76)69mhs*wbqb+l$1s&tw6!eoa1o%eh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

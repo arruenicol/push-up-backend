aa291d07dfab287819900122be804ed8134dcf5a
nicolo <arruenicole@gmail.com>
2025-05-16 00:38:16 -0400

    added dotenv in settings

--- notiff/settings.py
+++ notiff/settings.py
@@ -93,13 +93,14 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
+        'NAME': tmpPostgres.path.lstrip('/'),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
-        'PORT': 5432,
+        'PORT': tmpPostgres.port or 5432,
     }
 }
+
 
 
 # Password validation
